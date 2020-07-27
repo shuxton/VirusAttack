326887021648fa602146c5bd1f1658c9942fb2e4
shuxton <shubhamsnj@gmail.com>
2020-07-28 01:30:12 +0530

    respawnning assets and jump tile

--- src/game/game.js
+++ src/game/game.js
@@ -34,8 +34,18 @@ var config = {
 
  
 
-var platforms,player,virus,sides,bullet,mask,sanitizer,virusVel,key,enter,title;
-var nextFire=0,fireRate=100;
+var platforms,
+player,
+viral,
+sides,
+bullet,
+mask,
+sanitizer,
+virusVel,
+key,
+enter,
+title,
+nextFire=0,fireRate=200,score=0,oldScore=1,bulletCount=0,health=0,coins=10000,shieldTime=0,added=true,add1=false,add2=false;
 
 
 function preload() {
@@ -47,39 +57,40 @@ function preload() {
    this.load.multiatlas('idleL', 'assets/idleL_guy.json', 'assets');
    this.load.multiatlas('runL', 'assets/runL_guy.json', 'assets');
 
- this.load.image('background', 'assets/1624.jpg');
- this.load.image('title', 'assets/title.png');
- this.load.image('computer', 'assets/computer.png');
-
- this.load.image('enter', 'assets/enter.png');
- this.load.image('shield', 'assets/shield.png');
-
- this.load.image('mask', 'assets/mask.png');
- this.load.image('virus', 'assets/virus.png');
- this.load.image('portal', 'assets/portal.png');
- this.load.image('sanitizer', 'assets/sanitizer.png');
- this.load.image('bullet', 'assets/bullet.png');
-
- this.load.image('tile','assets/Tiles/Tile (2).png')
- this.load.image('tileJump','assets/Tiles/Tile (15).png')
- this.load.image('tileSides','assets/Tiles/Tile (5).png')
-
- this.load.image('acid','assets/Tiles/Acid (2).png')
+   this.load.image('background', 'assets/1624.jpg');
+   this.load.image('title', 'assets/title.png');
+   this.load.image('computer', 'assets/computer.png');
+   this.load.image('enter', 'assets/enter.png');
+   this.load.image('jump', 'assets/jump.png');
+   this.load.image('capsules', 'assets/capsules.png');
+   this.load.image('shield', 'assets/shield.png');
+   this.load.image('mask', 'assets/mask.png');
+   this.load.image('virus', 'assets/virus.png');
+   this.load.image('portal', 'assets/portal.png');
+   this.load.image('sanitizer', 'assets/sanitizer.png');
+   this.load.image('bullet', 'assets/bullet.png');
+   this.load.image('tile','assets/Tiles/Tile (2).png')
+   this.load.image('tileSides','assets/Tiles/Tile (5).png')
+   this.load.image('acid','assets/Tiles/Acid (2).png')
 }
 
 
 function create() {
+
   this.add.image(512,384, 'background');
+
+
   title=this.add.image(512,384,'title')
   enter=this.add.image(512,500,'enter').setScale(0.8,0.8)
-  //this.add.image(100,100,'computer').setScale(0.5,0.5)
 
   //this.sound.play('instruction');
  
   platforms = this.physics.add.staticGroup();
   sidesL = this.physics.add.staticGroup();
   sidesR = this.physics.add.staticGroup();
-  
+  jump= this.physics.add.staticGroup();
+  sanPlatform=this.physics.add.staticGroup();
+ 
 
 
 
@@ -93,40 +104,50 @@ function create() {
 
   platforms.create(130,838,'tile')
   platforms.create(380,838,'tile')
-   platforms.create(635,838,'acid')
- platforms.create(890,838,'tile')
-// platforms.create(890,568,'tileJump')
+  platforms.create(635,838,'acid')
+  platforms.create(890,838,'tile')
 
-this.physics.add.staticGroup().create(940,80,'portal')
-
- virus = this.physics.add.sprite(950,100,'virus');
- virus.body.allowGravity = false;
-
- //sanitizer=this.physics.add.sprite(950,100,'sanitizer');
+  this.physics.add.staticGroup().create(940,80,'portal')
 
 
 
- virus.setCollideWorldBounds(true);
+viral = this.physics.add.group({
+    key: 'virus',
+    repeat: 0,
+    setXY: { x: 950, y: 100, stepX: 0 }
+});
+viral.children.iterate(function (child) {
+
+    child.body.allowGravity = false;
+    child.setCollideWorldBounds(true);
+});
+
+
+sanitizer=this.physics.add.sprite(120,70,'sanitizer');
+sanitizer.visible=false;
+sanitizer.body.allowGravity=false;
+sanitizer.setCollideWorldBounds(true)
+
 
 
  mask=this.physics.add.sprite(950,100,'mask');
  mask.visible=false;
- mask.setCollideWorldBounds(true)
-
-  player = this.physics.add.sprite(100, 450, 'idle').setScale(0.2,0.2);
-
-  shield=this.physics.add.sprite(player.x,player.y,'shield').setScale(0.2,0.2)
+ mask.body.allowGravity=false;
+ mask.setCollideWorldBounds(true);
 
   
+ 
+ player = this.physics.add.sprite(100, 450, 'idle').setScale(0.2,0.2);
+ player.setBounce(0.2);
+ player.setCollideWorldBounds(true);
 
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  //shield.setCollideWorldBounds(true);
-  shield.body.allowGravity=false;
-shield.visible=false;
-
+ 
+ 
+ 
+ shield=this.physics.add.sprite(player.x,player.y,'shield').setScale(0.2,0.2)
+ shield.body.allowGravity=false;
+ shield.visible=false;
+  
 
   this.anims.create({
    key: 'left',
@@ -156,46 +177,69 @@ this.anims.create({
 
 
 this.physics.add.collider(player, platforms);
-//this.physics.add.collider(shield, platforms);
+this.physics.add.collider(player, sanPlatform);
+this.physics.add.collider(player, sanitizer,sanTouch,null,this);
+this.physics.add.collider(player, jump,highJump,null,this);
 
-maskcol=this.physics.add.collider(player, mask,maskChange,null,this);
+this.physics.add.collider(player, mask,maskChange,null,this);
+
+this.physics.add.collider(sanitizer, sanPlatform);
 this.physics.add.collider(platforms, mask);
+this.physics.add.collider(mask, jump);
 
-this.physics.add.collider(virus, platforms,virusChange,null,this);
-this.physics.add.collider(virus, sidesL,virusChangeL,null,this);
-this.physics.add.collider(virus, sidesR,virusChangeR,null,this);
+this.physics.add.collider(viral, platforms,virusChange,null,this);
+this.physics.add.collider(viral, sidesL,virusChangeL,null,this);
+this.physics.add.collider(viral, sidesR,virusChangeR,null,this);
 
-function maskChange(){
-    mask.visible=false;
-    maskcol.destroy();
-    shield.visible=true;
+function sanTouch(){
+    if(coins>=500){
+    bulletCount=10;
+    coins-=500;
+    sanitizer.destroy()
+    add1 =true;
+    }
 }
 
-function virusChange(){
+function highJump(){
+    player.setVelocityY(-650)
+}
+
+function maskChange(){
+    if(coins>=500){
+    coins-=500;
+    mask.visible=false;
+    mask.destroy();
+    shield.visible=true;
+    shieldTime=new Date().getTime();
+    add2=true
+    }
+}
+
+function virusChange(virus,platforms){
 
 if(virus.body.velocity.x==0)virus.setVelocityX((Math.random()*400+400));
 else virus.setVelocityX((virus.body.velocity.x))
 
-if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+400))
+if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+700))
 else     virus.setVelocityY(-(virus.body.velocity.y));
 
 }
 
-function virusChangeL(){
+function virusChangeL(virus,sidesL){
 
    if(virus.body.velocity.x==0)virus.setVelocityX((Math.random()*400+400));
    else virus.setVelocityX(-(virus.body.velocity.x))
   
-  if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+400))
+  if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+700))
   else     virus.setVelocityY((virus.body.velocity.y));
   }
 
-  function virusChangeR(){
+  function virusChangeR(virus,sidesR){
 
    if(virus.body.velocity.x==0)virus.setVelocityX(-(Math.random()*400+400));
    else virus.setVelocityX(-(virus.body.velocity.x))
   
-  if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+400))
+  if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+700))
   else     virus.setVelocityY(-(virus.body.velocity.y));
   }
 
@@ -223,33 +267,142 @@ cursors = this.input.keyboard.createCursorKeys();
 
 
 function update() {
+    if(add1 && add2)added=false;
+
+    if(shield.visible && new Date().getTime()-shieldTime>7000){
+        shield.visible=false;
+    }
+    if(score==oldScore){
+        oldScore=2*score;
+        if(oldScore>16){oldScore=2;score=1}
+        viral = this.physics.add.group({
+            key: 'virus',
+            repeat: (score*2)-1,
+            setXY: { x: 950, y: 100, stepX: 0 }
+        });
+        viral.children.iterate(function (child) {
+            child.setCollideWorldBounds(true);
+
+            child.setVelocityX(-(Math.random()*400+400));
+            child.setVelocityY(Math.random()*400+400)
+        
+        });score=0
+        this.physics.add.collider(viral, platforms,virusChange,null,this);
+this.physics.add.collider(viral, sidesL,virusChangeL,null,this);
+this.physics.add.collider(viral, sidesR,virusChangeR,null,this);
+function virusChange(virus,platforms){
+
+    if(virus.body.velocity.x==0)virus.setVelocityX((Math.random()*400+400));
+    else virus.setVelocityX((virus.body.velocity.x))
+    
+    if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+700))
+    else     virus.setVelocityY(-(virus.body.velocity.y));
+    
+    }
+    
+    function virusChangeL(virus,sidesL){
+    
+       if(virus.body.velocity.x==0)virus.setVelocityX((Math.random()*400+400));
+       else virus.setVelocityX(-(virus.body.velocity.x))
+      
+      if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+700))
+      else     virus.setVelocityY((virus.body.velocity.y));
+      }
+    
+      function virusChangeR(virus,sidesR){
+    
+       if(virus.body.velocity.x==0)virus.setVelocityX(-(Math.random()*400+400));
+       else virus.setVelocityX(-(virus.body.velocity.x))
+      
+      if(virus.body.velocity.y==0)virus.setVelocityY(-(Math.random()*400+700))
+      else     virus.setVelocityY(-(virus.body.velocity.y));
+      }
+    
+    }
+
+
     shield.setPosition(player.x,player.y)
+
+
+
     if(key.Enter.isDown && !this.gameStarted){
         this.gameStarted=true;
-       virus.body.allowGravity=true;
+        viral.children.iterate(function (child) {
+
+            child.body.allowGravity = true;
+        });
+       
        title.destroy();
        enter.destroy();
      mask.visible=true;
+     sanitizer.visible=true;
+    
+ jump.create(540,420,'jump')
+ jump.create(920,670,'jump')
+
+ sanPlatform.create(120,200,'jump')
+ sanitizer.body.allowGravity=true;
+ mask.body.allowGravity=true;
     }
     if(this.gameStarted){
         if(!virusVel){
-        virus.setVelocityX(-(Math.random()*400+400));
-virus.setVelocityY(Math.random()*400+400)
+            viral.children.iterate(function (child) {
+
+                child.setVelocityX(-(Math.random()*400+400));
+                child.setVelocityY(Math.random()*400+400)
+            
+            });
+        
 virusVel=true;
         }
         if(key.Space.isDown){
-            if(new Date().getTime()>nextFire ){
+            if(bulletCount>=0 && new Date().getTime()>nextFire ){
+                if(bulletCount>0){
+                bulletCount--;
                 nextFire = new Date().getTime() + fireRate;
-                bullet=this.physics.add.sprite(player.x, 590, 'bullet')
+                bullet=this.physics.add.sprite(player.x, player.y, 'bullet')
                 bullet.body.allowGravity=false;
             bullet.setVelocityX(660);
-            bulletCol=this.physics.add.collider(virus, bullet,bulletHit,null,this);
-function bulletHit(){
-virus.destroy();
-bulletCol.destroy();
+            bulletCol=this.physics.add.overlap(viral, bullet,bulletHit,null,this);
+            console.log(added)
+function bulletHit(virus,bullet){
+    score++;
+    bullet.disableBody(true,true);
+    virus.disableBody(true,true);
+//bulletCol.destroy();
+
 }
-            }
+            }else if(!added){
+                sanitizer=this.physics.add.sprite(120,70,'sanitizer');
+                sanitizer.setCollideWorldBounds(true)
+                this.physics.add.collider(player, sanitizer,sanTouch,null,this);
+                this.physics.add.collider(sanitizer, sanPlatform);
+                function sanTouch(){
+                    if(coins>=500){
+                    bulletCount=10;
+                    coins-=500;
+                    sanitizer.destroy()
+                   add1=true;
+                    }
+                }
+                mask=this.physics.add.sprite(950,100,'mask');
+                mask.setCollideWorldBounds(true)
+                this.physics.add.collider(player, mask,maskChange,null,this);
+                this.physics.add.collider(platforms, mask);
+                this.physics.add.collider(mask, jump);
+                function maskChange(){
+                    if(coins>=500){
+                    coins-=500;
+                    mask.visible=false;
+                   mask.destroy();
+                    shield.visible=true;
+                    shieldTime=new Date().getTime();
+                    add2=true;
+                    }
+                }added=true;add1=false;add2=false;
+            }}
         }
+       
    if (cursors.left.isDown)
    {
       
@@ -285,16 +438,13 @@ function reset(){
     this.gameStarted = false;
 	this.gameOver = false;
     this.score = 0;
-    this.virus.body.allowGravity=false;
+    viral.children.iterate(function (child) {
+
+        child.body.allowGravity = false;
+    });
 virusVel=false;
 }
 
-function start(){
-    this.gameStarted = true;
-	//this.gameOver = false;
-   // this.score = 0;
-    this.virus.body.allowGravity=true;
-//virusVel=true;
-}
+
 
 function render(){}
